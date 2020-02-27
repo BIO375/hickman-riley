60ea0115bb893e8cc9896bfeae4ba597772ab294
Riley Hickman <rileyhickman_2020@depauw.edu>
2020-02-27 08:04:53 -0500

    Start of Lab 4

---
+++ assignments/2020-02-27_Lab4.R
@@ -0,0 +1,16 @@
+# Lab 4
+
+# Clean up the working environment
+rm(list = ls())
+
+### Install and load packages ####
+
+if(!require(Rmisc)){install.packages("Rmisc")}
+if(!require(DescTools)){install.packages("DescTools")}
+if(!require(boot)){install.packages("boot")}
+if(!require(rcompanion)){install.packages("rcompanion")}
+if(!require(summarytools)){install.packages("summarytools")}
+if(!require(tidyverse)){install.packages("tidyverse")}
+
+# Check for updates
+tidyverse_update()
