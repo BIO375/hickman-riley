b8865f9a2a55659209ec359f6d9d215707ad313a
Riley Hickman <rileyhickman_2020@depauw.edu>
2020-03-12 10:24:02 -0400

    Working on Lab 5

---
+++ assignments/2020-03-12_Lab5.R
@@ -0,0 +1,20 @@
+# Lab 5
+
+rm(list = ls())
+
+### Install and load packages ####
+
+if(!require(Rmisc)){install.packages("Rmisc")}
+if(!require(DescTools)){install.packages("DescTools")}
+if(!require(boot)){install.packages("boot")}
+if(!require(rcompanion)){install.packages("rcompanion")}
+if(!require(summarytools)){install.packages("summarytools")}
+if(!require(tidyverse)){install.packages("tidyverse")}
+
+# Check for updates
+tidyverse_update()
+
+# R code for confidence interval
+# mean + c(-1, 1) *qt(1-alpha, df) *se
+
+
