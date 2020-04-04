e680c514f06fee3e5269a31a1dcac283268fa671
Riley Hickman <rileyhickman_2020@depauw.edu>
2020-04-04 14:10:21 -0500

    Done with 13-25. Still working.

--- assignments/2020-04-02_Lab6.R
+++ assignments/2020-04-02_Lab6.R
@@ -18,6 +18,8 @@ salmoncolor <- read_csv("datasets/abd/chapter13/chap13q20SalmonColor.csv")
 #      - Mann-Whitney U test is a method to use on a two sample test. It tells us if the two distributions are different.
 
 # B.) Is there a difference in mean between these two groups? Kokanee and sockeye? 
+#      Alternative = There is a difference in means.
+#      Null = There is no difference in means. 
 
 # Two sided t test
 t.test(skinColor ~ species, data = salmoncolor, var.equal = TRUE,
@@ -26,4 +28,20 @@ t.test(skinColor ~ species, data = salmoncolor, var.equal = TRUE,
 # Two sided Wilcoxan test
 wilcox.test(skinColor ~ species,
             data = salmoncolor, alternative = "two.sided", conf.level = 0.95)
+
+# P value seems to be really low so we reject the null. There does seem to be a difference in means between these groups. 
+
+### Problem 13-25 ####
+
+clearcuts <- read_csv("datasets/abd/chapter13/chap13q25Clearcuts.csv")
+
+# Alt: There is a difference in biomass
+# Null: there is no difference in biomass
+
+t.test(clearcuts$biomassChange, 
+       alternative = "two.sided", mu = 0, conf.level = 0.95)
+
+# P>0.05 so fail to reject null hypothesis. With this data we can't tell that there was a significant change in biomass.
+
+### Problem 13-26 ####
 
