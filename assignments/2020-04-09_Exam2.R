87f1fdc3e31f3c418e36e0f157b237ebe7e39efe
Riley Hickman <rileyhickman_2020@depauw.edu>
2020-04-11 12:50:18 -0500

    I think I'm done with exam 2

--- assignments/2020-04-09_Exam2.R
+++ assignments/2020-04-09_Exam2.R
@@ -17,6 +17,12 @@ library("DescTools")
 ### Question 17 ####
 baker <- read_csv("datasets/demos/baker.csv")
 
+baker <- baker %>%
+  mutate(diff = After - Before)
+
+ggplot(baker) +
+  geom_histogram(aes(diff), binwidth = 1)
+
 # One-sided, HA that after is greater than before
 t.test(baker$After, baker$Before, 
        alternative = "greater", paired =  TRUE, conf.level = 0.95)
@@ -27,5 +33,11 @@ install.packages("abd", repos="http://R-Forge.R-project.org")
 library("abd")
 
 algae <- AlgaeCO2
+
+ggplot(algae) +
+  geom_histogram(aes(growthrate), binwidth = .5)+
+  facet_wrap(~treatment)
+
+t.test(growthrate ~ treatment, data = algae, var.equal = TRUE, alternative = "two.sided", conf.level = 0.95)
 
 
