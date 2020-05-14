d711f9d135dd72bddefb35c75a58d0a6823a39b9
Riley Hickman <rileyhickman_2020@depauw.edu>
2020-05-14 14:40:07 -0500

    final exam

--- assignments/2020-05-14_FinalExam.R
+++ assignments/2020-05-14_FinalExam.R
@@ -26,20 +26,32 @@ insulation <- read_csv("datasets/demos/insulation.csv")
 # Null: Slope = 0  There will be no significant prediction of heat loss by leanness
 
 # Assumptions:
-# 
+# 1. at each value of x there is a population of possible y values whose mean lies on the true regression line
+# 2. at each value of x the distribution of possible y values is normal
+# 3. the variance of y values is the same at all values of x
+# 4. at each value of x the y measurements represent a random sample from possible y values
 
-model01 <- lm(heat_loss ~ leanness, data = insulation)
-autoplot(model01, smooth.colour = NA)
+modela <- lm(heat_loss ~ leanness, data = insulation)
+autoplot(modela, smooth.colour = NA)
 
-insulation_plus <- augment(model01)
+insulation_plus <- augment(modela)
 ggplot(data = insulation_plus)+
   geom_point(aes(x = leanness, y= .resid))
 
 
-# This plot does not look that great and nothing seems very linear about it. So I am going to do a transformation
+# This plot does not look that great and nothing seems very linear about it. Very fan shaped
+# So I am going to do a transformation of the y variable (heat_loss). 
+# A transformation is warranted 
 
 insulation <- insulation %>%
   mutate(sqrt_heat = sqrt(heat_loss))
-model02<-lm(sqrt_heat ~ leanness, data = insulation)
+modelb<-lm(sqrt_heat ~ leanness, data = insulation)
 ggplot(data = insulation)+
-  geom_point(aes(x = leanness, y= resid(model02)))
+  geom_point(aes(x = leanness, y= resid(modelb)))
+
+summary(modelb)
+
+# Boys with more fat quanity have significantly greater amount of heat loss:  
+# (Linear regression: sqrt(heat_loss) = 0.012 + 0.046 (leanness); df = 1, 10,
+# F=44.58, p<0.001). Fat quanity explained more than 80% variation in the square root transformed grain
+# number (R2 = 0.8168).
